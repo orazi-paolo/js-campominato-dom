b6d6b68e197cc079aa2babf1f5a79f3ae9fe49d1
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-26 16:37:44 +0200

    added milestone 4

--- js/script.js
+++ js/script.js
@@ -3,23 +3,21 @@ const buttonPlay = document.getElementById('play');
 const grid = document.getElementById('grid');
 const selectField = document.getElementById('difficulty');
 const scoreField = document.getElementById('score');
-// /////////////////////////////////////////////////////////////////////////
-// Preparo una variabile per tenere il punteggio dell utente
+
+// Preparo una variabile per tenere il punteggio dell'utente
 let score = 0;
 
 // Preparo la lista delle posizioni delle bombe
 let randomNumbersBomb = [];
-// Funzioni
 
 // Funzione per generare 16 numeri casuali tutti diversi compresi tra 1 e il totale delle celle
-function casualNumberBomb(totalCells){
+function casualNumberBomb(totalCells) {
     // resetto l'array
     randomNumbersBomb = [];
 
-    // fino a che la lunghezza dell array è inferiore a 16...
-        while (randomNumbersBomb.length < 16) {
-
-            // genero numero casuale
+    // fino a che la lunghezza dell'array è inferiore a 16...
+    while (randomNumbersBomb.length < 16) {
+        // genero numero casuale
         let randomNumber = Math.floor(Math.random() * totalCells) + 1;
 
         // se il numero non è presente nella lista lo inserisco
@@ -29,6 +27,7 @@ function casualNumberBomb(totalCells){
     }
     return randomNumbersBomb;
 }
+
 // Funzione per creare una singola cella
 function generatedCell(number, classCell) {
     const cell = document.createElement('div');
@@ -36,23 +35,29 @@ function generatedCell(number, classCell) {
     cell.textContent = number;
 
     // Quando clicco su una cella...
-    cell.addEventListener('click', function() {
+    cell.addEventListener('click', function () {
         console.log(this.textContent);
-        
+
         // converto la stringa della cella in un numero
         const cellNumber = parseInt(this.textContent);
 
-        //  controllo se il numero della cella cliccata è presente nell array
-        if(randomNumbersBomb.includes(cellNumber)){
+        // controllo se il numero della cella cliccata è presente nell'array
+        if (randomNumbersBomb.includes(cellNumber)) {
             cell.classList.add('bomb');
             console.log(`Hai trovato una bomba! Hai perso! Il tuo punteggio è di ${score}`);
-            // Se la cella non ha la classe selected allora aggiungi il punto su score 
-        }else if(!cell.classList.contains('selected')){
+        } else if (!cell.classList.contains('selected')) {
+            // Se la cella non ha la classe selected aggiungo il punto su score
             score++;
             scoreField.innerHTML = score;
+            this.classList.add('selected');
+
+            // Verifica condizione di vittoria
+            if (score === (cellNumber - randomNumbersBomb.length)) {
+                console.log(`HAI VINTO! Il tuo punteggio è di ${score}`);
+            }
         }
-        this.classList.add('selected');
     });
+
     return cell;
 }
 
@@ -60,21 +65,23 @@ function generatedCell(number, classCell) {
 function generatedGrid(numberCell, classCell) {
     // tolgo tutto dalla griglia per resettarla
     grid.innerHTML = '';
-    
+
     // resetto il punteggio
     score = 0;
     scoreField.innerHTML = score;
+
+    // genero i numeri delle bombe
     casualNumberBomb(numberCell);
-    console.log(casualNumberBomb(numberCell));
+    console.log(randomNumbersBomb);
+
     for (let i = 1; i <= numberCell; i++) {
         const cell = generatedCell(i, classCell);
         grid.appendChild(cell);
     }
 }
-// /////////////////////////////////////////////////////////////////////////
 
 // Quando clicco su play genero la griglia con le celle
-buttonPlay.addEventListener('click', function() {
+buttonPlay.addEventListener('click', function () {
     const difficulty = selectField.value;
     let numberCells;
     let classCell;
